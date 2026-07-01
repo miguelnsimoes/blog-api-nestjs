54df73790812c0e5c22994ca2bfb59dc9dcb2ca0
miguelnsimoes <miguelnazariosimoes@gmail.com>
2026-07-01 19:19:03 -0300

    chore: configurar conexão com o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+
+        database: config.get<string>('DB_DATABASE'),
+
+        autoLoadEntities: true,
+
+        synchronize: true,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
