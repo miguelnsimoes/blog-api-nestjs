4c4ba31c0e14afd25b26b51e280a6b8110a71829
miguelnsimoes <miguelnazariosimoes@gmail.com>
2026-07-05 20:39:15 -0300

    feat: ocultar senha nas respostas da API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import {
+  ValidationPipe,
+  ClassSerializerInterceptor,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(
+      app.get(Reflector),
+    ),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
